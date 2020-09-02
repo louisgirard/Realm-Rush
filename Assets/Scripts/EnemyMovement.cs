013c67f4f922566c28d35b8334507cd6f21202bc
louisgirard <louis.girard17@hotmail.com>
2020-09-02 11:39:30 +0200

    enemy follow a simple path

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -1,17 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-    [SerializeField] List<Block> path;
+    [SerializeField] List<Waypoint> path;
 
     // Start is called before the first frame update
     void Start()
     {
-        foreach (Block block in path)
+        StartCoroutine(FollowPath());
+    }
+
+    IEnumerator FollowPath()
+    {
+        print("Starting patrol");
+        foreach (Waypoint waypoint in path)
         {
-            print(block.name);
+            print("Visiting block: " + waypoint.name);
+            transform.position = waypoint.transform.position;
+            yield return new WaitForSeconds(1f);
         }
+        print("Ending patrol");
     }
 
     // Update is called once per frame
--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Block : MonoBehaviour
+public class Waypoint : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
