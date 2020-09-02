052e0eeb59ef2c08facb2123d71fdd1e4032a173
louisgirard <louis.girard17@hotmail.com>
2020-09-02 11:03:54 +0200

    add text mesh on top of cube + change text with coordinates on grid

---
+++ Assets/CubeEditor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+public class CubeEditor : MonoBehaviour
+{
+    [Range(1f, 20f)] [SerializeField] float gridSize = 10f;
+
+    TextMesh textMesh;
+
+    void Update()
+    {
+        Vector3 snapPosition;
+        snapPosition.x = Mathf.RoundToInt(transform.position.x / gridSize) * gridSize;
+        snapPosition.y = 0f;
+        snapPosition.z = Mathf.RoundToInt(transform.position.z / gridSize) * gridSize;
+
+        transform.position = snapPosition;
+
+        textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = snapPosition.x.ToString() + "," + snapPosition.z.ToString();
+    }
+}
