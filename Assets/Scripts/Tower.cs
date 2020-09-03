a776b4c7d426accfb3e4075fcaab3175483e739d
louisgirard <louis.girard17@hotmail.com>
2020-09-03 17:07:06 +0200

    towers shoot at enemy when in range

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,11 +5,22 @@ using UnityEngine;
 public class Tower : MonoBehaviour
 {
     [SerializeField] Transform head;
-    [SerializeField] Transform targetEnemy;
+    [SerializeField] float attackRange = 30f;
+    [SerializeField] ParticleSystem barrel;
+
+    Transform targetEnemy;
 
     // Update is called once per frame
     void Update()
     {
         head.LookAt(targetEnemy);
+        ShootAtEnemy();
+    }
+
+    private void ShootAtEnemy()
+    {
+        targetEnemy = FindObjectOfType<Enemy>().transform;
+        float distance = Vector3.Distance(targetEnemy.position, head.position);
+        barrel.gameObject.SetActive(distance <= attackRange);
     }
 }
